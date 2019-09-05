5805693221191ec816e7d09797a2416da86cf0d6
zubeir-Abubakar <zubkayare@gmail.com>
2019-09-05 12:48:31 +0300

    updating the readme.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# MY_PORTFOLIO
+#### A portfolio and about me website, 05/09/19
+#### By **Zubeyr Abubakr**
+## Description
+This website describes whos I am, highlights some of my work as a Web Developer, my skills, my background and my contact info if you want to hire me!
+
+## Technologies Used
+- HTML
+- CSS
+- JS
+- Bootstrap
+- jQuery
+- FormSpree
+## Support and contact detail
+ZUBEYR ABUBAKR https://github.com/zubeir-Abubakar/
+### License
+This project is under MIT License
+Copyright (c) 2019 Zubeyr Abubakar
---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+(function($) {
+
+    "use strict";
+
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    },
+
+    $WIN = $(window);
+
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+
+// Menu on Scrolldown
+    var clMenuOnScrolldown = function() {
+
+        var menuTrigger = $('.header-menu-toggle');
+
+        $WIN.on('scroll', function() {
+
+            if ($WIN.scrollTop() > 150) {
+                menuTrigger.addClass('opaque');
+            }
+            else {
+                menuTrigger.removeClass('opaque');
+            }
+
+        });
+    };
+
+
+// OffCanvas Menu
+    var clOffCanvas = function() {
+
+        var menuTrigger     = $('.header-menu-toggle'),
+            nav             = $('.header-nav'),
+            closeButton     = nav.find('.header-nav_close'),
+            siteBody        = $('body'),
+            mainContents    = $('section, footer');
+
+        // open-close menu by clicking on the menu icon
+        menuTrigger.on('click', function(e){
+            e.preventDefault();
+            // menuTrigger.toggleClass('is-clicked');
+            siteBody.toggleClass('menu-is-open');
+        });
+
+        // close menu by clicking the close button
+        closeButton.on('click', function(e){
+            e.preventDefault();
+            menuTrigger.trigger('click');
+        });
+
+        // close menu clicking outside the menu itself
+        siteBody.on('click', function(e){
+            if( !$(e.target).is('.header-nav, .header-nav_content, .header-menu-toggle, .header-menu-toggle span') ) {
+                // menuTrigger.removeClass('is-clicked');
+                siteBody.removeClass('menu-is-open');
+            }
+        });
+
+    };
+
+    /* Back to Top
+    * ------------------------------------------------------ */
+    var clBackToTop = function() {
+
+        var pxShow  = 500,         // height on which the button will show
+        fadeInTime  = 400,         // how slow/fast you want the button to show
+        fadeOutTime = 400,         // how slow/fast you want the button to hide
+        scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        goTopButton = $(".go-top")
+
+        // Show or hide the sticky footer button
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() >= pxShow) {
+                goTopButton.fadeIn(fadeInTime);
+            } else {
+                goTopButton.fadeOut(fadeOutTime);
+            }
+        });
+    };
+
+
+    /* Initialize
+    * ------------------------------------------------------ */
+    (function ssInit() {
+
+        clMenuOnScrolldown();
+        clOffCanvas();
+        clBackToTop();
+
+    })();
+
+
+
+})(jQuery);
